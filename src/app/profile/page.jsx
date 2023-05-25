ac9df272d538cea3ef8e2bceda6853335b218e4a
dev-rome <haynesjerome@gmail.com>
2023-05-25 09:59:17 -0400

    added edit and delete functions

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -4,11 +4,13 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-import UserProfile from "../../components/UserProfile"
+import UserProfile from "../../components/UserProfile";
 
 const Profile = () => {
   const [posts, setPosts] = useState([]);
+
   const { data: session } = useSession();
+  const router = useRouter();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -20,8 +22,27 @@ const Profile = () => {
     if (session?.user.id) fetchPosts();
   }, [session?.user.id]);
 
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  
+  const handleDelete = async (post) => {
+    const confirmed = confirm("Are you sure you want to delete this post?");
+
+    if (confirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <UserProfile
