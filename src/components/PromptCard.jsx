78104d1d430c547e1e2de244e1567d2133e1c7aa
dev-rome <haynesjerome@gmail.com>
2023-05-25 11:22:02 -0400

    added profile click function

--- src/components/PromptCard.jsx
+++ src/components/PromptCard.jsx
@@ -3,11 +3,12 @@
 import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
-import Image from "next/image";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
+import Image from "next/image";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
+
   const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
@@ -18,10 +19,18 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     setTimeout(() => setCopied(""), 3000);
   };
 
+  const handleProfileClick = () => {
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex flex-1 justify-start items-center gap-3 cursor-pointer">
+        <div
+          className="flex flex-1 justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
           <Image
             src={post.creator.image}
             alt="user_image"
