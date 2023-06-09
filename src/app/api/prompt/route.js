a74bd586a8b180d3418fa4c901934ded8d01e3ee
dev-rome <haynesjerome@gmail.com>
2023-06-08 20:59:58 -0400

    edit route file

--- src/app/api/prompt/route.js
+++ src/app/api/prompt/route.js
@@ -2,12 +2,24 @@ import { connectToDB } from "../../../../utils/database";
 import Prompt from "../../../../models/prompt";
 
 export const GET = async (req) => {
+  const url = new URL(req.url);
+  const searchParams = url.searchParams;
+  const search = searchParams.get("search") || "";
+  const regex = new RegExp(search, "i");
+
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find({}).populate("creator");
+    const prompts = await Prompt.find({prompt: {$regex: regex}}).populate("creator");
 
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    const headers = {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache, no-store, must-revalidate",
+      Expires: "0",
+      Pragma: "no-cache",
+    }
+
+    return new Response(JSON.stringify(prompts), { status: 200, headers });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
